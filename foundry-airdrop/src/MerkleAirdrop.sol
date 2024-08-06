d887d20da5c0ffcbfdb42773ce16103e80611433
gianluca cattin <gcattin97@gmail.com>
2024-08-06 15:15:00 +0200

    getter functions

--- foundry-airdrop/src/MerkleAirdrop.sol
+++ foundry-airdrop/src/MerkleAirdrop.sol
@@ -8,10 +8,12 @@ contract MerkleAirdrop {
   using SafeERC20 for IERC20;
 
   error MerkleAirdrop__InvalidProof();
+  error MerkleAirdrop__AlreadyClaimed();
 
   address[] claimers;
   bytes32 private immutable i_merkleRoot;
   IERC20 private immutable i_airdropToken;
+  mapping(address claimer => bool claimed) private s_hasClaimed;
 
   event Claim(address account, uint256 amount);
 
@@ -21,11 +23,23 @@ contract MerkleAirdrop {
   }
 
   function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+    if(s_hasClaimed[account]) {
+      revert MerkleAirdrop__AlreadyClaimed();
+    }
     bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encodePacked(account, amount))));
     if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
       revert MerkleAirdrop__InvalidProof();
     }
+    s_hasClaimed[account] = true;
     emit Claim(account, amount);
     i_airdropToken.safeTransfer(account, amount);
+  }
+
+  function getMerkleRoot() external view returns (bytes32) {
+    return i_merkleRoot;
+  }
+
+  function getAirdropToken() external view returns (IERC20) {
+    return i_airdropToken;
   }
 }
